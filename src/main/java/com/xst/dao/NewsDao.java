3d077c4990807feaeae40fcff0429f78492345b3
CrazyCodess <1312480794@qq.com>
2016-02-27 20:51:28 +0800

    news first commit

--- src/main/java/com/xst/dao/NewsDao.java
+++ src/main/java/com/xst/dao/NewsDao.java
@@ -1,7 +1,11 @@
 package com.xst.dao;
 
 import com.xst.entity.V9News;
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 
@@ -13,5 +17,10 @@ public class NewsDao  extends BaseDao {
 
 	public V9News getById(int id){
 		return get(V9News.class,id);
+	}
+	public List<V9News> queryForNewsList(){
+		String hql="from V9News as news order by news.listorder desc,news.updatetime desc";
+		Query query=query(hql);
+		return query.list();
 	}
 }
