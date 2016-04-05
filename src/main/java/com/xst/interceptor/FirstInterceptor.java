e99d5fbded31a2f43641e5bd2bf51ecf94a53558
JetMuffin <564936642@qq.com>
2016-04-05 14:53:35 +0800

    cj:修改环境变量注入方法

--- src/main/java/com/xst/interceptor/FirstInterceptor.java
+++ src/main/java/com/xst/interceptor/FirstInterceptor.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 /**
  * 注入全局变量
@@ -24,22 +25,14 @@ public class FirstInterceptor extends HandlerInterceptorAdapter {
 
         //isHasGlobalVar：用于判断是否已经拦截过
         if(session.getAttribute("isHasGlobalVar") == null){
+            session.setAttribute("isHasGlobalVar", true);
 
-            System.out.println("进入拦截器...");
-
-            session.setAttribute("isHasGlobalVar",1);
-            session.setAttribute("assetsPath",globalVar.assetsPath);
-            session.setAttribute("upFilePath",globalVar.upFilePath);
-
-
+            Map<String, String> variables = globalVar.getAll();
+            for (Map.Entry<String, String> entry : variables.entrySet()) {
+                session.setAttribute(entry.getKey(), entry.getValue());
+            }
             System.out.println(session.getAttribute("index"));
-
         }
-        System.out.println("entry.....");
-
-
         return  true;
-
     }
-
 }
