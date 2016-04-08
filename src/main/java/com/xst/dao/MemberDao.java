85a12cb52a20b41da085d3114ae527b89e78463c
shilan910 <910323478@qq.com>
2016-04-08 10:57:08 +0800

    zqh:注册初步

--- src/main/java/com/xst/dao/MemberDao.java
+++ src/main/java/com/xst/dao/MemberDao.java
@@ -1,9 +1,13 @@
 package com.xst.dao;
 
 import com.xst.entity.V9Member;
+import com.xst.entity.V9MemberVerify;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by sl on 16-3-12.
@@ -17,6 +21,38 @@ public class MemberDao extends BaseDao{
 
     public void addMember(V9Member member){
         save(member);
+    }
+
+
+
+    /**
+     * 判断邮箱是否存在，V9Member
+     * @param email
+     * @return
+     */
+    public boolean isEmailExit(String email){
+
+        if(HQuery("email",email) == null){
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+    /**
+     * 查询
+     * @param colume
+     * @param value
+     * @return
+     */
+    private List<V9Member> HQuery(String colume , String value){
+        String hql = "from V9Member as member where member."+colume+"=?";
+        Query query = query(hql);
+        query.setString(0, String.valueOf(value));
+        List<V9Member> results = query.list();
+        return results;
     }
 
 }
---
+++ src/main/java/com/xst/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.xst.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by sl on 16-4-8.
+ */
+
+@Repository("userDao")
+public class UserDao {
+
+
+
+}
