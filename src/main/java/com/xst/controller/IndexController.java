0f26ef93864dcfb1f6cfe477cb5f525d16cf9da8
shilan910 <910323478@qq.com>
2016-04-15 09:08:48 +0800

    zqh:20160415-1

--- src/main/java/com/xst/controller/IndexController.java
+++ src/main/java/com/xst/controller/IndexController.java
@@ -11,9 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.xst.entity.V9Admin;
-import com.xst.dao.AdminDao;
-
 @Controller
 @RequestMapping("/")
 public class IndexController {
